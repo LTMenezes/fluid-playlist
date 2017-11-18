13e41747e6562b35f9d02172d125c869894b1b15
LTMenezes <leonardotmenezes@gmail.com>
2017-11-18 20:20:29 -0200

    Add project readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Fluid Playlist
+
+## About
+
+A Library to build fluid playlists in spotify.
+
+## Authors
+- Leonardo Teixeira Menezes
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'fluid-playlist',
+  packages = ['fluid-playlist'],
+  version = '0.1.0',
+  description = 'A library to build fluid playlists in Spotify.',
+  author = 'Leonardo Menezes',
+  author_email = 'leonardotmenezes@gmail.com',
+  url = 'https://github.com/LTMenezes/fluid-playlist',
+  download_url = 'https://github.com/LTMenezes/fluid-playlist/archive/master.zip',
+  keywords = ['fluid', 'playlist', 'spotify'],
+  classifiers = [],
+)
